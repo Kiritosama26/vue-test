e1dae7b6e6b4e4ecccc9b338d7eef4ac3c0b6193
Keiven <keivenpadilla@outlook.com>
2025-04-19 15:27:06 -0500

    ultima update agregue componente en otro lado y lo llame en appvue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,21 +1,9 @@
 <template>
-  <h1>Hola mundo</h1>
-  <p>{{ contador }}</p>
-  <button @click="aumentar" class="bg-danger">Incrementar</button>
-  <button @click="decrementar">Decrementar</button>
+ <Contador/>
 </template>
 
 <script setup>
-import {ref} from 'vue';
-const contador = ref(0);
-
-const aumentar=()=>{
-  contador.value++;
-}
-
-const decrementar = () => {
-  contador.value--;
-}
+import Contador from './components/Contador.vue';
 </script>
 
 
---
+++ vue-project/src/components/Contador.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hola mundo</h1>
+  <p>{{ contador }}</p>
+  <button @click="aumentar" class="bg-danger">Incrementar</button>
+  <button @click="decrementar">Decrementar</button>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const contador = ref(0);
+
+const aumentar=()=>{
+  contador.value++;
+}
+
+const decrementar = () => {
+  contador.value--;
+}
+</script>
+
+
+<style scoped>
+
+</style>
